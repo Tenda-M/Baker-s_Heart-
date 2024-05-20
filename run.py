90f953987769ee5cdb79997a936fcdd84120860c
Tatenda Mudehwe <tmudehwe@hotmail.com>
2024-05-20 15:54:35 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -172,6 +172,25 @@ def get_last_5_entries_sales():
         columns.append(column[-5:])
 
     return columns
+##############################
+
+# function we’ll pass it the stock_data that our get_last_5_entries_sales function returned.
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column] # converst the valuses into integers
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
 
 ##############################    
 #mains functions, with function calls at the end
@@ -184,6 +203,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
+    
 
 
 print("Welcome to Baker's Heart Data Automation")
@@ -191,5 +214,5 @@ print("Welcome to Baker's Heart Data Automation")
 this print  statement is the first thing we see,  
 before the functions inside the main function are called. 
 """
-#main()
-sales_columns = get_last_5_entries_sales()
+main()
+
